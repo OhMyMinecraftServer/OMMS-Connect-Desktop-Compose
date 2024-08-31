7f83b98275af4c7c8342fd882f067a18b3852b40
MercuryGryph <w84627302@gmail.com>
2024-08-31 18:18:11 +0800

    移除不必要/无用的构建参数

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,19 +57,16 @@ compose.desktop {
             licenseFile.set(project.file("LICENSE"))
 
             windows {
-                jvmArgs += "-Dfile.encoding=GBK"
                 iconFile.set(project.file("icon.ico"))
                 dirChooser = true
                 menuGroup = "OhMyMinecraftServer"
             }
 
             macOS {
-                jvmArgs += "-Dfile.encoding=UTF-8"
                 iconFile.set(project.file("icon.icns"))
             }
 
             linux {
-                jvmArgs += "-Dfile.encoding=UTF-8"
                 iconFile.set(project.file("icon.png"))
             }
         }
