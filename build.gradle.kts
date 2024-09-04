3fc726cac5f16fc17df57f905637ff611c408f2c
MercuryGryph <w84627302@gmail.com>
2024-09-04 17:23:47 +0800

    排除不需要的模块

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,9 @@ dependencies {
     // compose.desktop.currentOs should be used in launcher-sourceSet
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs)
+    implementation(compose.desktop.currentOs) {
+        exclude(group = "org.jetbrains.compose.material")
+    }
     implementation(compose.components.resources)
     implementation(compose.material3)
 
